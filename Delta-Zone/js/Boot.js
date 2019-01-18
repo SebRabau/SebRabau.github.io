113ff305e08ce30bd4c987e53dd7a0e27db53ae5
Seb Rabau <strabau@gmail.com>
2019-01-18 20:06:40 +0000

    update

--- Delta-Zone/js/Boot.js
+++ Delta-Zone/js/Boot.js
@@ -6,7 +6,7 @@ DeltaZone.Boot.prototype = {
   preload: function() {
   	//assets used in the loading screen
     this.load.image("logo", "assets/logo.png");
-    this.load.image("preloadBar", "assets/PreloadSBar.png");
+    this.load.image("preloadBar", "assets/PreloadBar.PNG");
   },
   create: function() {
     this.game.stage.backgroundColor = "#fff";
